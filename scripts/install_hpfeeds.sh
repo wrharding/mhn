1f7daf4e4a142ea84bd2fe1f2e9cb10790a936b0
wrharding <57780704+wrharding@users.noreply.github.com>
2023-04-16 17:48:47 +0000

    Added python3 install; Updated hpf broker supervisorctl

--- scripts/install_hpfeeds.sh
+++ scripts/install_hpfeeds.sh
@@ -9,11 +9,13 @@ MHN_HOME=$SCRIPTS/..
 if [ -f /etc/debian_version ]; then
     apt-get -y update
     # this needs to be installed before calling "which pip", otherwise that command fails
-    apt-get -y install libffi-dev build-essential python-pip python-dev git libssl-dev supervisor
+    apt-get -y install libffi-dev build-essential python-pip python-dev python3 python3-pip git libssl-dev supervisor
 
     PYTHON=`which python`
+    PYTHON3=`which python3`
     PIP=`which pip`
-    $PIP install virtualenv
+    PIP3=`which pip3`
+    $PIP3 install virtualenv
     VIRTUALENV=`which virtualenv`
 
 else
@@ -25,7 +27,7 @@ ldconfig /usr/local/lib/
 
 bash install_mongo.sh
 
-$PIP install virtualenv
+$PIP3 install virtualenv
 
 mkdir -p /opt
 cd /opt
@@ -33,7 +35,7 @@ rm -rf /opt/hpfeeds
 git clone https://github.com/hpfeeds/hpfeeds.git
 chmod 755 -R hpfeeds
 cd hpfeeds
-$VIRTUALENV -p $PYTHON env
+$VIRTUALENV -p $PYTHON3 env
 . env/bin/activate
 
 pip install cffi
@@ -46,10 +48,20 @@ mkdir -p /var/log/mhn
 mkdir -p /etc/supervisor/
 mkdir -p /etc/supervisor/conf.d
 
+cat >> /opt/hpfeeds/users.json <<EOF
+{
+  "my-user-ident": {
+    "owner": "my-owner",
+    "secret": "my-really-strong-passphrase",
+    "subchans": ["chan1"],
+    "pubchans": ["chan2"],
+  }
+}
+EOF
 
 cat >> /etc/supervisor/conf.d/hpfeeds-broker.conf <<EOF 
 [program:hpfeeds-broker]
-command=/opt/hpfeeds/env/bin/python /opt/hpfeeds/broker/feedbroker.py
+command=/opt/hpfeeds/env/bin/python /opt/hpfeeds/env/bin/hpfeeds-broker -e tcp:port=10000 --exporter=0.0.0.0:9431 --auth=/opt/hpfeeds/users.json
 directory=/opt/hpfeeds
 stdout_logfile=/var/log/mhn/hpfeeds-broker.log
 stderr_logfile=/var/log/mhn/hpfeeds-broker.err
